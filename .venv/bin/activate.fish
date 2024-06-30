efd938838a04f5b7071b776ee6fc94f3ed3b5ee1
Alina Khan <khanalinaa000@gmail.com>
2024-06-30 21:20:07 +0530

    Initial commit

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alina26/ML-Projects/nn_deploy/.venv"
+set -gx VIRTUAL_ENV "/home/alina26/ML-Projects/nn_deployy/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
